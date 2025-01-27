a2e99f4baf23b9695547aab3d5697bef1803f964
benshabbat <benshabbat27@gmail.com>
2025-01-27 13:53:16 +0200

    ok route for root

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 import express from 'express';
-
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import RouteRoot from './routes/root.js';
 
 const app = express();
 const PORT = process.env.PORT ||8800;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/",RouteRoot);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
