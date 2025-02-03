77239939e1fd1f40b8273f6b3c383d530739064b
benshabbat <benshabbat27@gmail.com>
2025-02-03 09:31:21 +0200

    app.all for 404

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ const __dirname = dirname(__filename);
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/",RouteRoot);
+app.all("*", (req, res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,"views", "404.html"))
+    }
+    else if(req.accepts('json')){
+        res.json({message: "Not Found"})
+    }
+    else{
+        res.type('txt').send("404 Not Found")
+    }
+)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
